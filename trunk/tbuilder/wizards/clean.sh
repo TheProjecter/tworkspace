e10c3c2f670b4f9eecb3b8ff2acdf7940eea7541
tigran.job <tigran.job@8eb44cc9-8d7a-f8a1-6560-d94b4c19051a>
2010-11-19 22:38:14 +0000

    Author: Tigran Hovhannisyan
    Improved cleaning mechanism to do not output something if there is nothing to remove.

--- trunk/tbuilder/wizards/clean.sh
+++ trunk/tbuilder/wizards/clean.sh
@@ -6,20 +6,19 @@ txtrst='\e[0m'    # Text Reset
 txtblu='\e[0;34m' # Blue
 bldgrn='\e[1;32m' # Green
 
-echo -e "${txtblu}Cleaning... ${txtrst}" 
-if [ "$1" != "" ]; then 
+if [ -d "$1" ]; then 
         echo -n -e " ${bldblk}> cleaning object files...${txtrst}"
         rm -fr $1
         echo -e "${bldgrn}done.${txtrst}"
 fi
 
-if [ "$2" != "" ]; then 
+if [ -d "$2" ]; then 
         echo -n -e " ${bldblk}> cleaning executables...${txtrst}"
         rm -fr $2
         echo -e "${bldgrn}done.${txtrst}"
 fi
 
-if [ "$3" != "" ]; then 
+if [ -f "$3/run" ]; then 
         echo -n -e " ${bldblk}> cleaning generated scripts...${txtrst}"
         rm -f $3/run
         echo -e "${bldgrn}done.${txtrst}"
