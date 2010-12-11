d5c1a412afbd434b08dc2c485f6f6ba1cce357e2
tigran.job <tigran.job@8eb44cc9-8d7a-f8a1-6560-d94b4c19051a>
2010-12-11 20:41:06 +0000

    Author: Tigran Hovhannisyan
    Removed old tworkspace to replace it with the new one.

--- trunk/tbuilder/core/clean.mk
+++
@@ -1,9 +0,0 @@
-
-.PHONY : clean
-
-clean:
-	@bash $(BUILDER_ROOT)/wizards/clean.sh \
-		$(OBJ_PATH) \
-		$(BIN_PATH) \
-		$(DEV_ROOT) \
-		$(BUILDER_ROOT)/wizards/colors.sh
--- trunk/tbuilder/core/test.mk
+++
@@ -1,7 +0,0 @@
-
-.PHONY: test
-test:
-	@bash $(BUILDER_ROOT)/wizards/test.sh \
-		$(BUILDER_ROOT) \
-		$(DEV_ROOT) \
-		$(BUILDER_ROOT)/wizards/colors.sh
