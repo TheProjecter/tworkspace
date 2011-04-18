0ebe485aa833768b32fb673ade829c6ff30b44be
tigran.job <tigran.job@8eb44cc9-8d7a-f8a1-6560-d94b4c19051a>
2011-04-18 14:49:05 +0000

    Author: Tigran Hovhannisyan
    Added static library creation mechanizm.

--- trunk/product/src/project_1/unit_tests/main.cpp
+++ trunk/product/src/project_1/unit_tests/main.cpp
@@ -1,5 +1,15 @@
 
+#include <dlfcn.h>
+#include <iostream>
+
 int main()
 {
+	void* h = dlopen("/u/thovhann/Desktop/gnu_make/tworkspace/trunk/product/src/project_1/i686_linux_2.6.9/debug/lib_project_1.so", RTLD_LAZY);
+	if (!h ) {
+		std::cerr << "Cannot open library: " << dlerror() << "\n";
+		return 1;
+	}
+	dlclose(h);
 	return 0;
 }
+
